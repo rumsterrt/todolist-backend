cb3ecc01d860a879cd8d49dfd93e1528ed689bdf
Anatoly Ryumkin <rumsterrt@gmail.com>
2020-01-06 03:15:19 +0900

    Add CORS

--- config.example.js
+++ config.example.js
@@ -1,5 +1,10 @@
 module.exports = {
-    API_DB_CONNECTION_SETTINGS: 'host=localhost&user=root&password=password&database=TestDB&port=3306',
     API_ENDPOINT: 4000,
     API_IS_LOCAL: true,
+    DATABASE_PORT: 3306,
+    DATABASE_HOST: 'localhost',
+    DATABASE_NAME: 'todo_db',
+    DATABASE_USER: 'root',
+    DATABASE_ACCESS_KEY: 'GodLike3133',
+    CLIENT_URL: 'http://localhost:3000',
 }
--- index.js
+++ index.js
@@ -2,9 +2,21 @@ require('./initEnvironments')
 const express = require('express')
 const bodyParser = require('body-parser')
 const routers = require('./server/routers')
+const cors = require('cors')
 
 const app = express()
 
+app.use(
+    cors({
+        origin: (origin, callback) => {
+            if (process.env.CLIENT_URL.indexOf(origin) !== -1) {
+                callback(null, true)
+            } else {
+                callback(new Error('Not allowed by CORS'))
+            }
+        },
+    }),
+)
 app.use(bodyParser.json())
 
 routers.forEach(item => app.use('/' + item.name, item.router))
